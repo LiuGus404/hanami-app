5cc20954112b9637753c1ac38117237231c017b4
LiuGus404 <liugushk@gmail.com>
2025-05-21 23:49:52 +0800

    app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,22 @@
 export const metadata = {
-  title: 'Hanami Web',
-  description: 'Hanami Web Application',
+  title: 'Hanami Music',
+  description: 'Hanami Music Web App',
   manifest: '/manifest.json',
   icons: {
     apple: [
-      { url: '/icon/icon-152x152.png', sizes: '152x152', type: 'image/png' },
-      { url: '/icon/icon-144x144.png', sizes: '144x144', type: 'image/png' },
-      { url: '/icon/icon-128x128.png', sizes: '128x128', type: 'image/png' },
-      { url: '/icon/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icon/icon-384x384.png', sizes: '384x384', type: 'image/png' },
-      { url: '/icon/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/icons/Hanami.png', sizes: '512x512', type: 'image/png' }
     ],
     other: [
-      { url: '/icon/icon-96x96.png', sizes: '96x96', type: 'image/png' },
-      { url: '/icon/icon-72x72.png', sizes: '72x72', type: 'image/png' },
+      { url: '/icons/Hanami.png', sizes: '512x512', type: 'image/png' }
     ],
-    shortcut: ['/icon/favicon.ico'],
+    shortcut: ['/icons/Hanami.png'],
   },
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
-    title: 'Hanami Web',
+    title: 'Hanami Music',
     startupImage: [
-      '/icon/icon-512x512.png'
+      '/icons/Hanami.png'
     ]
   },
   viewport: {
@@ -31,5 +25,5 @@ export const metadata = {
     maximumScale: 1,
     userScalable: false
   },
-  themeColor: '#FFFFFF',
+  themeColor: '#fffaf5',
 } 
