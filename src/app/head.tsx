e1fc2a7cb1b0d444589957f83c2e191bfef4d46d
LiuGus404 <liugushk@gmail.com>
2025-05-27 22:51:47 +0800

    hi

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,12 +2,56 @@ export default function Head() {
     return (
       <>
         <title>Hanami Music</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="theme-color" content="#fffaf5" />
+        <meta name="description" content="Hanami Music - 讓我們一起陪孩子快樂成長" />
+        
+        {/* PWA manifest */}
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
+        
+        {/* Safari/iOS specific */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Hanami Music" />
-        {/* 你也可以加 favicon 或其他 meta */}
+        
+        {/* Icons for iOS */}
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon-72.png" />
+        <link rel="apple-touch-icon" sizes="96x96" href="/icons/icon-96.png" />
+        <link rel="apple-touch-icon" sizes="128x128" href="/icons/icon-128.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/icon-144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-180.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192.png" />
+        
+        {/* Splash Screens for iOS */}
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)"
+          href="/splash/iPhone_14_Pro_Max_landscape.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
+          href="/splash/iPhone_14_Pro_landscape.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
+          href="/splash/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png"
+        />
+        
+        {/* Favicon */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-16.png" />
+        <link rel="shortcut icon" type="image/png" href="/icons/icon-192.png" />
+        
+        {/* Safari specific */}
+        <link rel="mask-icon" href="/icons/icon-512.png" color="#fffaf5" />
+        
+        {/* Microsoft */}
+        <meta name="msapplication-TileColor" content="#fffaf5" />
+        <meta name="msapplication-TileImage" content="/icons/icon-144.png" />
       </>
     );
   }
