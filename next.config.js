92c58f5fc2bdfb9f5e171a27fa4977e45e4d531f
LiuGus404 <liugushk@gmail.com>
2025-05-28 00:26:48 +0800

    change

--- next.config.js
+++ next.config.js
@@ -12,4 +12,10 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig)
